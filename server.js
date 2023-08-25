42a4472597d483d17acef24a383d831d0dff6dc8
Shahidul <shahid.cse.19@gmail.com>
2023-08-25 17:39:01 +0600

    update server.ja

--- server.js
+++ server.js
@@ -26,13 +26,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookiePerser());
 
 const PORT = process.env.PORT || 5000
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-  });
+
 //connection to the database
 connectDB();
 
